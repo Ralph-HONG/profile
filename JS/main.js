d83243f8ea652b2a5bc6e0f7b09af38b5d60d566
hsens <60877421+hsens@users.noreply.github.com>
2020-05-31 23:42:04 +0900

    navbar 애니메이션 추가

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,15 @@
 'use strict'
 
+
+const navbar = document.querySelector('#navbar');
+const navbarheight = navbar.getBoundingClientRect().height;
+
 document.addEventListener('scroll', () => {
-    console.log(window.screenY);
+    console.log(window.scrollY);
+    console.log(navbarheight);
+    if (window.scrollY > navbarheight) {
+        navbar.classList.add('navbar_dark');
+    } else {
+        navbar.classList.remove('navbar_dark');
+    }
 });
