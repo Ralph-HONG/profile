62f16167b8a3b74250b2ac01cf03b5abc257e268
hsens <60877421+hsens@users.noreply.github.com>
2020-06-14 16:57:38 +0900

    javascript home contactBtn

--- JS/main.js
+++ JS/main.js
@@ -4,12 +4,40 @@
 const navbar = document.querySelector('#navbar');
 const navbarheight = navbar.getBoundingClientRect().height;
 
+
+
+// 스크롤y가 navbarheight보다 내려가면 클래스를 추가 
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(navbarheight);
     if (window.scrollY > navbarheight) {
         navbar.classList.add('navbar_dark');
     } else {
         navbar.classList.remove('navbar_dark');
     }
 });
+
+
+
+const navbarMenu = document.querySelector('.navbar_menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null){
+        return;
+    }
+    scrollIntoViews(link);
+});
+
+
+const contentMeBtn = document.querySelector('.home_contact');
+contentMeBtn.addEventListener('click', () => {
+    scrollIntoViews('#contact');
+});
+
+
+
+
+// 해당 창으로 이동시키는 함수
+function scrollIntoViews(seletor) {
+    const scrollTo = document.querySelector(seletor);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+}
