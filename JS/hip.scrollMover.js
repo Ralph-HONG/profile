c28e9ae35e63c29cb005fade3a70a83bf9c54cf3
Ralph <dev.inpyo@gmail.com>
2020-10-06 12:59:10 +0900

    자바스크립트 스크롤 기능 모듈링

---
+++ JS/hip.scrollMover.js
@@ -0,0 +1,35 @@
+
+(function(){
+
+  if(window.hip == null){
+    window.hip = {};
+  }
+
+  let public_func = {
+    target:null
+  };
+
+  var config = {
+    speed:"smooth"
+  };
+
+  public_func.with = function (seletor, spd){
+    public_func.target = $(seletor);
+    if(spd != null){
+      config.speed = spd;
+    }
+    public_func.target.bind("click", function (e){
+      if(this.dataset.link != null){
+        public_func.scroll(this.dataset.link);
+      }
+    });
+  };
+
+  // 해당 창으로 이동시키는 함수
+  public_func.scroll = function (seletor) {
+    const scrollTo = document.querySelector(seletor);
+    scrollTo.scrollIntoView({behavior: config.speed});
+  }
+
+  window.hip.scrollMover = public_func;
+})();
