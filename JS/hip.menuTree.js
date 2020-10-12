a9ac6279c6628394eebcc12988d670e5e1a1f7d6
Ralph <dev.inpyo@gmail.com>
2020-10-12 18:27:15 +0900

    메뉴 트리구조 객체화(진행중)

--- JS/hip.menuTree.js
+++ JS/hip.menuTree.js
@@ -1,54 +1,52 @@
 (function () {
 
-  if (window.hip == null) {
-    window.hip = {};
-  }
+      if (window.hip == null) {
+        window.hip = {};
+      }
 
-  let public_func = {
-    target: null
-  };
+      let public_func = {
+        target: null
+      };
 
 
-  public_func.load = function (query, index) {
-    public_func.target = $(query);
-    let $menu = public_func.target;
+      public_func.load = function (query, index) {
+        public_func.target = $(query);
 
-    index.forEach(function (value) {
-      var menuKey = Object.keys(value);
-      var objVal = value[menuKey];
-      if (objVal.val == null) {
-        console.log(menuKey + " : " + objVal);
-      } else {
-        index.forEach(function (value) {
-          var menuKey = Object.keys(value);
-          var objVal = value[menuKey];
+        for (var i = 0; i < index.length; i++) {
+          if (index[i].length > 1) {
+            for (var j = 0; j < index[i][j].length; j++) {
+              console.log(index[j]);
+            }
+          } else {
+            console.log(index[i]);
+
+          }
         }
       }
-    });
-  };
 
-  //    public_func.select(index);
-  //    public_func.target.find('a').bind('click', function (e) {
-  //      $menu.find('li a').removeClass('active');
-  //      $(e.target).addClass('active');
-  //      $(e.target).parents('li').find('> a').addClass('active');
-  //    });
-  //  };
-  //
-  //  public_func.select = function (index) {
-  //
-  //    let $menu = public_func.target;
-  //    $menu.find('li a').removeClass('active');
-  //    $menu.find('li:nth('+index+') > a').addClass('active');
-  //  };
-  //
-  //
-  // public_func.render = function () {
-  //    let $menu = public_func.target;
-  //    $menu.find('li > ul').hide();
-  //
-  //    $menu.find('li.active > ul').show();
-  //  };
+//    public_func.select(index);
+//    public_func.target.find('a').bind('click', function (e) {
+//      $menu.find('li a').removeClass('active');
+//      $(e.target).addClass('active');
+//      $(e.target).parents('li').find('> a').addClass('active');
+//    });
+//  };
+//
+//  public_func.select = function (index) {
+//
+//    let $menu = public_func.target;
+//    $menu.find('li a').removeClass('active');
+//    $menu.find('li:nth('+index+') > a').addClass('active');
+//  };
+//
+//
+// public_func.render = function () {
+//    let $menu = public_func.target;
+//    $menu.find('li > ul').hide();
+//
+//    $menu.find('li.active > ul').show();
+//  };
 
-  window.hip.menuTree = public_func;
-})();
+      window.hip.menuTree = public_func;
+    }
+)();
