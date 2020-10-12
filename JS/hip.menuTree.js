1cc39e0b26179da5bc5b29ede3c465c3c52d61db
Ralph <dev.inpyo@gmail.com>
2020-10-12 13:27:14 +0900

    메뉴트리 모듈링 완성

--- JS/hip.menuTree.js
+++ JS/hip.menuTree.js
@@ -5,16 +5,36 @@
   }
 
   let public_func = {
-    target:null
+    target: null
   };
 
 
-  public_func.load = function() {
+  public_func.load = function (query, index) {
+    public_func.target = $(query);
+    let $menu = public_func.target;
 
-    $('.menu>a').click(function () {
-      $(this).next("ul").toggleClass("hide");
+    public_func.select(index);
+    public_func.target.find('a').bind('click', function (e) {
+      $menu.find('li a').removeClass('active');
+      $(e.target).addClass('active');
+      $(e.target).parents('li').find('> a').addClass('active');
     });
-  }
+  };
+
+  public_func.select = function (index) {
+
+    let $menu = public_func.target;
+    $menu.find('li a').removeClass('active');
+    $menu.find('li:nth('+index+') > a').addClass('active');
+  };
+
+
+ public_func.render = function () {
+    let $menu = public_func.target;
+    $menu.find('li > ul').hide();
+
+    $menu.find('li.active > ul').show();
+  };
 
   window.hip.menuTree = public_func;
-  })();
+})();
