b2c70a3e9fa4c312a0446241a341e874d2d1d32c
Ralph <dev.inpyo@gmail.com>
2020-10-12 15:48:48 +0900

    메뉴 트리구조 객체화

--- JS/hip.menuTree.js
+++ JS/hip.menuTree.js
@@ -13,28 +13,42 @@
     public_func.target = $(query);
     let $menu = public_func.target;
 
-    public_func.select(index);
-    public_func.target.find('a').bind('click', function (e) {
-      $menu.find('li a').removeClass('active');
-      $(e.target).addClass('active');
-      $(e.target).parents('li').find('> a').addClass('active');
+    index.forEach(function (value) {
+      var menuKey = Object.keys(value);
+      var objVal = value[menuKey];
+      if (objVal.val == null) {
+        console.log(menuKey + " : " + objVal);
+      } else {
+        index.forEach(function (value) {
+          var menuKey = Object.keys(value);
+          var objVal = value[menuKey];
+        }
+      }
     });
   };
 
-  public_func.select = function (index) {
-
-    let $menu = public_func.target;
-    $menu.find('li a').removeClass('active');
-    $menu.find('li:nth('+index+') > a').addClass('active');
-  };
-
-
- public_func.render = function () {
-    let $menu = public_func.target;
-    $menu.find('li > ul').hide();
-
-    $menu.find('li.active > ul').show();
-  };
+  //    public_func.select(index);
+  //    public_func.target.find('a').bind('click', function (e) {
+  //      $menu.find('li a').removeClass('active');
+  //      $(e.target).addClass('active');
+  //      $(e.target).parents('li').find('> a').addClass('active');
+  //    });
+  //  };
+  //
+  //  public_func.select = function (index) {
+  //
+  //    let $menu = public_func.target;
+  //    $menu.find('li a').removeClass('active');
+  //    $menu.find('li:nth('+index+') > a').addClass('active');
+  //  };
+  //
+  //
+  // public_func.render = function () {
+  //    let $menu = public_func.target;
+  //    $menu.find('li > ul').hide();
+  //
+  //    $menu.find('li.active > ul').show();
+  //  };
 
   window.hip.menuTree = public_func;
 })();
