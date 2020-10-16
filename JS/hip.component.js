b524f8bce13d2655ce70037467d210ff25426458
Ralph <dev.inpyo@gmail.com>
2020-10-16 17:16:21 +0900

    인풋박스 여러 파라미터 받아서 처리(이병덕 풀이)

--- JS/hip.component.js
+++ JS/hip.component.js
@@ -1,103 +1,129 @@
 (function () {
   if (window.hip == null) {
-    window.hip = {};
+    window.hip = {
+      component: {}
+    };
+  }
+  if (window.hip.component == null) {
+    window.hip.component = {};
   }
 
   let public_func = {
-    query: null
-    , input: {}
-    , option:{
-      required:false
-      ,phone:false
-      ,number:false
-      ,email:false
-      ,jumin:false
-    }
+    callback: {}
   };
 
-  let val = ''
-
-  public_func.input.load = function (query) {
-    public_func.query = query;
-    let selector = $(query);
-    let dataOpt = selector.attr("data-option");
-    let optList = dataOpt.split(',');
-    if(optList != null && optList.length > 0) {
-      for (let i = 0; i < optList.length; i++) {
-        let opt = optList[i];
-        public_func.option[opt] = true;
-      }
-    }
-
-    selector.bind("keypress", function (e) {
-      let val = $(e.target).val();
-      if (public_func.isValid(val + e.key, true)) {
-
-      } else {
-        e.preventDefault();
+  $(document).ready(function () {
+    $("[data-component]").each(function (idx, elem) {
+      public_func.load(elem);
+      let dataComponent = $(elem).attr('data-component');
+      if (dataComponent != null && dataComponent !== "") {
+        $(elem).bind('blur', function (e) {
+          let val = $(elem).val();
+          let callback = public_func.callback[dataComponent];
+          if (callback !== null && callback['blur'] !== null) {
+            callback['blur'](elem, val, e);
+          }
+        });
       }
     });
+  });
 
-    // if (opt.prevObject[0].dataset.option === "idNumber") {
-    //   let frontNum = selector.val()
-    //   if (frontNum.length >= 8) {
-    //     frontNum += frontNum.val() + '-';
-    //   }
-    // }
+  public_func.getOpt = function (elem) {
+    let dataOpt = $(elem).attr("data-option");
+    let optList = dataOpt.split(',');
+    let option = {
+      required: false
+      , phone: false
+      , numberalpha: false
+      , number: false
+      , email: false
+      , jumin: false
+      , alpha: false
+      , hangul: false
+      , spacial: false
+    };
+    if (optList != null && optList.length > 0) {
+      for (let i = 0; i < optList.length; i++) {
+        let opt = optList[i];
+        option[opt] = true;
+      }
+    }
+    return option;
   }
 
-  public_func.inspect = function (){
-    let val = $(public_func.query).val();
-    const result = public_func.isValid(val, false);
-    if(!result){
-      $(public_func.query).css("border", "1px solid #f00");
-    } else {
-      $(public_func.query).css("border", "inherit");
-    }
-  };
-
-  public_func.isValid = function (val, checkNow){
-    if(checkNow === true) {
-      if(public_func.option.number === true){
-        let ret = (val === val.replace(/[^0-9]/g, ""));
-        console.log("val :"+ val + ' valid : ' + ret);
-        return ret;
-      }
-    } else {
-      let ret = true;
-      if(public_func.option.required === true){
-        ret = !(val == null || val === '');
-      }
-      if(public_func.option.number === true){
-        let ret = (val === val.replace(/[^0-9]/g, ""));
-        console.log("val :"+ val + ' valid : ' + ret);
-        return ret;
-      }
-      if(public_func.option.jumin === true){
-        let ret = (val === val.replace(/[^0-9]/g, ""));
-        return (ret.length === 13);
+  public_func.load = function () {
+    if (arguments !== null && arguments.length > 0) {
+      for (let i = 0; i < arguments.length; i++) {
+        const inputQuerys = arguments[i];
+        $(arguments[i]).bind("keypress", function (e) {
+          let val = $(e.target).val();
+          if (public_func.isValid(inputQuerys, val + e.key, true)) {
+          } else {
+            e.preventDefault();
+          }
+        });
       }
     }
 
-    return true;
-  };
+    public_func.event = function (componentID, eventType, func) {
+      let callbackInfo = {};
+      callbackInfo[eventType] = func;
+
+      if (public_func.callback[componentID] == null) {
+        public_func.callback[componentID] = callbackInfo;
+      } else {
+        public_func.callback[componentID][eventType] = func;
+      }
+    };
 
 
-  public_func.input.submit = function (id, pw, phone, email) {
-    let loginId = $('#id').val();
-    let loginPw = $('#pw').val();
-    let loginPhone = $('#phone').val();
-    let loginEmail = $('#email').val();
+    //들어온 데이터 옵션별 입력제한
+    public_func.isValid = function (elem, val, checkNow) {
+      const option = public_func.getOpt(elem);
+      if (checkNow === true) {
+        if (option.number === true) {
+          let ret = (val === val.replace(/[^0-9]/g, ""));
+          console.log("val :" + val + ' valid : ' + ret);
+          return ret;
+        }
+        if (option.alpha === true) {
+          let ret = (val === val.replace(/[^a-zA-Z]/g, ""));
+          console.log("val :" + val + ' valid : ' + ret);
+          return ret;
+        }
+        if (option.numberalpha === true) {
+          let ret = (val === val.replace(/[^0-9a-zA-Z]/g, ""));
+          console.log("val :" + val + ' valid : ' + ret);
+          return ret;
+        }
+      } else {
+        let ret = true;
+
+        if (option.required === true) {
+          ret = !(val === null || val === '');
+        } else if (option.number === true) {
+          let ret = (val === val.replace(/[^0-9]/g, ""));
+          console.log("val :" + val + ' valid : ' + ret);
+          return ret;
+        } else if (option.jumin === true) {
+          let ret = (val === val.replace(/[^0-9]/g, ""));
+          return (ret.length === 13);
+        }
+      }
+      return true;
+    };
 
 
-    public_func.input.chkEmail = function (loginEmail) {
-      let regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-      return regExp.test(loginEmail);
-    }
-    console.log('ID : ' + loginId + '\n', 'PW : ' + loginPw + '\n', 'PHONE : ' + loginPhone + '\n', 'E-MAIL : ' + loginEmail + '\n');
+    public_func.inspect = function (elem) {
+      let val = $(elem).val();
+      const result = public_func.isValid(elem, val, false);
+      if (!result) {
+        $(elem).css("border", "1px solid #f00");
+      } else {
+        $(elem).css("border", "inherit");
+      }
+    };
+
   }
-  window.hip.component = public_func;
+  window.hip.component.input = public_func;
 })();
-
-
-
