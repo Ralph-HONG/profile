8c3282df5c24f6b34561483e303fbf43d05af8f1
Ralph <dev.inpyo@gmail.com>
2020-10-20 13:14:13 +0900

    text 토글 이벤트

---
+++ JS/hip.toggleText.js
@@ -0,0 +1,28 @@
+(function () {
+
+  if (window.hip == null) {
+    window.hip = {};
+  }
+
+
+  let public_func = {
+    target: null
+  }
+
+
+  public_func.load = function (query, voca) {
+    let elem = $(query);
+    let idx = 0;
+
+    elem.bind('click', function () {
+      if (idx > voca.length - 1) {
+        idx = 0;
+      }
+      elem.text(voca[idx++]);
+    });
+  };
+
+
+  window.hip.toggleText = public_func;
+}
+)();
