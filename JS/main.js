ba09b411e35eef225ad6a8e35b215e79bfe682bf
Ralph-HONG <dev.inpyo@gmail.com>
2021-06-24 23:50:33 +0900

    css 리팩토링

--- JS/main.js
+++ JS/main.js
@@ -1,43 +1,38 @@
 'use strict'
 
-
 const navbar = document.querySelector('#navbar');
 const navbarheight = navbar.getBoundingClientRect().height;
 
 
-
-// 스크롤y가 navbarheight보다 내려가면 클래스를 추가 
+// 스크롤y가 navbarheight보다 내려가면 클래스를 추가
 document.addEventListener('scroll', () => {
-    if (window.scrollY > navbarheight) {
-        navbar.classList.add('navbar_dark');
-    } else {
-        navbar.classList.remove('navbar_dark');
-    }
+	if (window.scrollY > navbarheight) {
+		navbar.classList.add('navbar_dark');
+	} else {
+		navbar.classList.remove('navbar_dark');
+	}
 });
-
 
 
 const navbarMenu = document.querySelector('.navbar_menu');
 navbarMenu.addEventListener('click', (event) => {
-    const target = event.target;
-    const link = target.dataset.link;
-    if (link == null){
-        return;
-    }
-    scrollIntoViews(link);
+	const target = event.target;
+	const link = target.dataset.link;
+	if (link != null) {
+		scrollIntoViews(link);
+	}
+	return false;
 });
 
 
 const contentMeBtn = document.querySelector('.home_contact');
 contentMeBtn.addEventListener('click', () => {
-    scrollIntoViews('#contact');
+	scrollIntoViews('#contact');
 });
-
-
 
 
 // 해당 창으로 이동시키는 함수
 function scrollIntoViews(seletor) {
-    const scrollTo = document.querySelector(seletor);
-    scrollTo.scrollIntoView({behavior: "smooth"});
+	const scrollTo = document.querySelector(seletor);
+	scrollTo.scrollIntoView({behavior: "smooth"});
 }
